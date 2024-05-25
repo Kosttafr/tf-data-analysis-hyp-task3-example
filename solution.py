4f2b4a114fe069406348cf56193f4217c6f0d2b7
311 Spirin Kirill <82974251+Kosttafr@users.noreply.github.com>
2024-05-25 23:04:39 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import numpy as np
+from statsmodels.stats.weightstats import ztest as ztest
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 748421383 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(data: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    alpha = 0.04
+    # Рассчитываем t-статистику и p-значение
+    t_stat, p_value = ztest(data, value=100) # используем z test
+    
+    # Сравниваем p-значение с уровнем значимости
+    if p_value < alpha:
+        return True  # Отклоняем нулевую гипотезу
+    else:
+        return False  # Не отклоняем нулевую гипотезу
